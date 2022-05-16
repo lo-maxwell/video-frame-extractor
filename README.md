c686ecfdeb004a084d44cccc03db467617b1e663
Maxwell Lo <lo-maxwell@berkeley.edu>
2022-05-15 21:59:57 -0700

    edit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# video-frame-extractor
+# video-frame-extracter
+python script to convert mp4 to jpg
---
+++ extracter.py
@@ -0,0 +1,29 @@
+# source env/bin/activate
+#python3 extracter.py
+
+import cv2
+import time
+import os
+
+path = "sxf"
+print("setting up directory for path {0}".format(path))
+os.system("rm -rf {0}".format(path))
+os.system("mkdir {0}".format(path))
+
+start = time.time()
+print("starting timer")
+vidcap = cv2.VideoCapture('sxf.mp4')
+success,image = vidcap.read()
+count = 0
+while success and count < 100:
+  cv2.imwrite("sxf/frame%d.jpg" % count, image)     # save frame as JPEG file
+  success,image = vidcap.read()
+  # print('Read a new frame: ', str(count) if success else 'ERROR')
+  count += 1
+
+
+end = time.time()
+print(end - start)
+f = open("sxf/stats.txt", "w")
+f.write("Total frames: \n{0}\nTime taken: \n{1}\n".format(count, str(end - start)))
+f.close()
